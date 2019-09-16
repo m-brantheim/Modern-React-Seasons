edbe947425ce92f4ac40be8e56916037385f20b4
Magnus Brantheim <m.brantheim@gmail.com>
2019-09-16 18:39:52 +0200

    Refactor and changed App to use SeasonDisplay component to display season

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,10 +1,16 @@
 import React from 'react'
 
-const SeasonDisplay = props => {
-    if (props.isNorthernHemisphere) {
-        return <div>North</div>
+const getSeason = (lat, month) => {
+    if (month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter'
+    } else {
+        return lat > 0 ? 'winter' : 'summer'
     }
-    return <div>South</div>
+}
+
+const SeasonDisplay = props => {
+    const season = getSeason(props.lat, new Date().getMonth())
+    return <div>{season}</div>
 }
 
 export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -3,24 +3,26 @@ import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-    constructor(props) {
-        super(props)
 
-        this.state = { lat: null, errorMessage: '' }
+    state = { lat: null, errorMessage: '' }
 
+    componentDidMount() {
         navigator.geolocation.getCurrentPosition(
             position => this.setState({ lat: position.coords.latitude }),
             err => this.setState({ errorMessage: err.message })
         )
     }
+    
     render() {
-        return (
-        <div>
-            Latitude: {this.state.lat}
-            <br />
-            Error: {this.state.errorMessage}
-        </div>
-        )
+        if (this.state.errorMessage && !this.state.lat) {
+            return <div>Error: {this.state.errorMessage}</div>
+        }
+
+        if (!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        }
+
+        return <div>Loading...</div>
     }
 }
 
