9eda44dcf2e7dab396792c6eb0192f17d4ad6f39
Magnus Brantheim <m.brantheim@gmail.com>
2019-09-18 17:52:07 +0200

    Added Spinner component

---
+++ src/Spinner.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Spinner = props => {
+    return (
+        <div className='ui active dimmer'>
+            <div className="ui big text loader">
+                {props.loadingText}
+            </div>
+        </div>
+    )
+}
+
+Spinner.defaultProps = {
+    loadingText: 'Loading...'
+}
+
+export default Spinner
